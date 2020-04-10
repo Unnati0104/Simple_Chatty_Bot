f5c8a461ff2202be8807dff862e77655d081dbe9
Dell <unnati.goyal_cs18@gla.ac.in>
2020-04-10 17:11:14 +0530

    Taking a number input and printing count

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,19 +10,27 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        String yourName = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have," + yourName + "!");
+        System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
-        int yourAge = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        System.out.println("Your age is" + yourAge + "that's a good time to start programming!");
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+
+        System.out.println("Completed, have a nice day!");
     }
 }
