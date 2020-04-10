3bc7fd1f48465b2ee9099e57c3ed3317ad41a186
Dell <unnati.goyal_cs18@gla.ac.in>
2020-04-10 16:20:17 +0530

    Printing date of birth statement

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -3,6 +3,7 @@ package bot;
 public class SimpleBot {
     public static void main(String[] args) {
         // write your code here
-    System.out.println("Hello! My name is Unnati");
+        System.out.println("Hello! My name is Unnati.");
+        System.out.println("I was created in 2000.");
     }
 }
